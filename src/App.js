c92eb4ed162e5836546c5c00612eedb2cc145aa2
avinashdarde <136440191+avinashdarde@users.noreply.github.com>
2025-08-22 18:42:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -435,7 +435,7 @@ function Modal({ isOpen, onClose, template }) {
           rel="noopener noreferrer" 
           className="w-full mt-2 flex items-center justify-center gap-3 bg-gradient-to-r from-pink-500 to-orange-500 text-white font-bold py-3 px-6 rounded-lg hover:opacity-90 transition-all duration-300"
         >
-          <Instagram size={20}/> Chat on Instagram
+          <Instagram size={20}/> Chat on Instagram to Customise
         </a>
     
 </div>
